96cebccb95ee661535d493e32748c074c472ad87
Program132 <clement.gauthier132@orange.fr>
2023-05-21 20:36:11 +0200

    Update Main.java

--- src/main/java/fr/program/Main.java
+++ src/main/java/fr/program/Main.java
@@ -1,6 +1,18 @@
 package fr.program;
-public class Main {
-    public static void main(String[] args) {
 
+import fr.program.utils.CommandsManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("[ModUtils] Ready");
+        CommandsManager.init(this);
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[ModUtils] Stopped");
     }
 }
