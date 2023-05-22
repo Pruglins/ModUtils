671a9668fe5969ea6b90ca53a3d71280abba3242
Program132 <clement.gauthier132@orange.fr>
2023-05-22 20:56:39 +0200

    Fixs.

--- src/main/java/fr/program/cmds/ModTool.java
+++ src/main/java/fr/program/cmds/ModTool.java
@@ -28,26 +28,41 @@ public class ModTool implements CommandExecutor {
                 Inventory playerInv = player.getInventory();
                 playerInv.clear();
                 FileConfiguration config = plugin.getConfig();
-                if (config.getString("modt." + player.getUniqueId()) == "on") {
-                    config.set("modt." + player.getUniqueId(), "off");
-                } else {
-                    config.set("modt." + player.getUniqueId(), "on");
 
-                    ItemCustom vanish = new ItemCustom("§l§9Vanish", Material.IRON_HOE, 1, true);
+                // Config de base dans le modt :
+                config.set("modt." + player.getUniqueId() + ".vanish", "off");
+
+                if (Objects.equals(config.getString("modt." + player.getUniqueId() + ".enabled"), "on")) {
+                    config.set("modt." + player.getUniqueId() + ".enabled", "off");
+                    plugin.saveConfig();
+
+                    player.sendMessage("[" + ChatColor.GOLD + "ModUtils" + ChatColor.WHITE + "] "
+                            + "Vous ne disposez plus des outils de modération.");
+                    return true;
+                } else {
+                    config.set("modt." + player.getUniqueId() + ".enabled", "on");
+                    plugin.saveConfig();
+
+                    ItemCustom vanish = new ItemCustom("§l§9Vanish", Material.IRON_HOE, 1, true, plugin, "vanish");
+                    //vanish.addKey(plugin, "vanish");
                     ItemCustom stick_kb_1 = new ItemCustom("§l§9KB 1", Material.STICK, 1, Enchantment.KNOCKBACK, 1);
                     ItemCustom stick_kb_2 = new ItemCustom("§l§9KB 2", Material.STICK, 1, Enchantment.KNOCKBACK, 2);
-                    ItemCustom cps = new ItemCustom("§l§9CPS", Material.CLOCK, 1, true);
-                    ItemCustom info_plr = new ItemCustom("§l§9Informations Joueurs", Material.PLAYER_HEAD, 1, true);
 
-                    playerInv.setItem(1, vanish);
-                    playerInv.setItem(3, stick_kb_1);
-                    playerInv.setItem(4, stick_kb_2);
-                    playerInv.setItem(6, cps);
-                    playerInv.setItem(7, info_plr);
+                    ItemCustom cps = new ItemCustom("§l§9CPS", Material.CLOCK, 1, true, plugin, "cps");
+                    ItemCustom info_plr = new ItemCustom("§l§9Informations Joueur", Material.PLAYER_HEAD, 1, true, plugin, "info_plr");
+
+                    playerInv.setItem(1, vanish.getItem());
+                    playerInv.setItem(3, stick_kb_1.getItem());
+                    playerInv.setItem(4, stick_kb_2.getItem());
+                    playerInv.setItem(6, cps.getItem());
+                    playerInv.setItem(7, info_plr.getItem());
 
                     player.updateInventory();
+
+                    player.sendMessage("[" + ChatColor.GOLD + "ModUtils" + ChatColor.WHITE + "] "
+                            + "Vous disposez des outils de modération.");
                 }
-                plugin.saveConfig();
+                return true;
             }
         }
         return false;
