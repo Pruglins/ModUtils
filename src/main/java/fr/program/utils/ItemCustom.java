ea47ec4f8455dbcdc2cd3596b706009e052f5c25
Program132 <clement.gauthier132@orange.fr>
2023-05-22 20:57:42 +0200

    Fix Custom Class for ItemStack

--- src/main/java/fr/program/utils/ItemCustom.java
+++ src/main/java/fr/program/utils/ItemCustom.java
@@ -1,23 +1,35 @@
 package fr.program.utils;
 
+import fr.program.Main;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
+import java.security.Key;
 import java.util.List;
 
-public class ItemCustom extends ItemStack {
-    public ItemCustom(String name, Material mat, int amount) {
-        super(mat, amount);
+public class ItemCustom {
+    private final ItemStack item;
+
+    public ItemCustom(String name, Material mat, int amount, boolean hideAttributes) {
+        this.item = new ItemStack(mat, amount);
         setDisplayName(name);
+        if (hideAttributes) {
+            disableAttributes();
+        } else {
+            enableAttributes();
+        }
     }
 
-    public ItemCustom(String name, Material mat, int amount, boolean hideAtttributes) {
-        super(mat, amount);
+    public ItemCustom(String name, Material mat, int amount, boolean hideAttributes, Main pluginInstance, String keyName) {
+        this.item = new ItemStack(mat, amount);
         setDisplayName(name);
-        if (hideAtttributes) {
+        addKey(pluginInstance, keyName);
+        if (hideAttributes) {
             disableAttributes();
         } else {
             enableAttributes();
@@ -25,53 +37,61 @@ public class ItemCustom extends ItemStack {
     }
 
     public ItemCustom(String name, Material mat, int amount, Enchantment enchantment, int level) {
-        super(mat, amount);
+        this.item = new ItemStack(mat, amount);
         setDisplayName(name);
         addEnchantment(enchantment, level);
     }
 
-    public ItemCustom(String name, Material mat, int amount, Enchantment enchantment, int level, boolean hideAtttributes) {
-        super(mat, amount);
-        setDisplayName(name);
-        addEnchantment(enchantment, level);
-        if (hideAtttributes) {
-            disableAttributes();
-        } else {
-            enableAttributes();
-        }
+    public ItemStack getItem() {
+        return this.item;
+    }
+
+    public ItemMeta getItemMeta() {
+        return this.item.getItemMeta();
+    }
+
+    public void setItemMeta(ItemMeta meta) {
+        this.item.setItemMeta(meta);
+    }
+
+    public void addKey(Main pluginInstance, String keyName) {
+        ItemMeta meta = this.item.getItemMeta();
+        NamespacedKey key = new NamespacedKey(pluginInstance, keyName);
+        meta.getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, Math.PI);
+        this.item.setItemMeta(meta);
     }
 
     public void addEnchantment(Enchantment ench, int level) {
-        ItemMeta meta = this.getItemMeta();
+        ItemMeta meta = this.item.getItemMeta();
         meta.addEnchant(ench, level, false);
-        this.setItemMeta(meta);
+        this.item.setItemMeta(meta);
     }
 
     public void addEnchantments(List<Enchantment> enchantments, List<Integer> levels) {
-        ItemMeta meta = this.getItemMeta();
+        ItemMeta meta = this.item.getItemMeta();
         for (int i = 0; i < enchantments.size(); i++) {
             Enchantment enchantment = enchantments.get(i);
             int level = levels.get(i);
             meta.addEnchant(enchantment, level, true);
         }
-        this.setItemMeta(meta);
+        this.item.setItemMeta(meta);
     }
 
     public void setDisplayName(String name) {
-        ItemMeta meta = this.getItemMeta();
+        ItemMeta meta = this.item.getItemMeta();
         meta.setDisplayName(name);
-        this.setItemMeta(meta);
+        this.item.setItemMeta(meta);
     }
 
     public void disableAttributes() {
-        ItemMeta meta = this.getItemMeta();
+        ItemMeta meta = this.item.getItemMeta();
         meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        this.setItemMeta(meta);
+        this.item.setItemMeta(meta);
     }
 
     public void enableAttributes() {
-        ItemMeta meta = this.getItemMeta();
+        ItemMeta meta = this.item.getItemMeta();
         meta.removeItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        this.setItemMeta(meta);
+        this.item.setItemMeta(meta);
     }
 }
